493b5a6a262d38cc4bb57a756b3d4a8f9f8d0080
_natwoj <info@nwojcik.com>
2021-10-02 23:46:30 +0200

    play button to pause

--- js.js
+++ js.js
@@ -1,3 +1,16 @@
 let playButton = document.getElementById("playButton");
 let pauseButton = document.getElementById("pauseButton");
 let videoPlayer = document.getElementById("videoPlayer");
+
+function changePlayButtonToPauseButton() {
+    playButton.classList.remove("play-containter-visible-on");
+    playButton.classList.add("play-containter-visible-off");
+
+    pauseButton.classList.remove("pause-container-visible-off");
+    pauseButton.classList.add("pause-container-visible-on");
+
+    doPlayVideo();
+    function doPlayVideo(){
+        videoPlayer.play();
+    }
+}
