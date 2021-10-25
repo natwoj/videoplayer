a5a93b9294e881102013fc952c37999131a8e915
natwoj <79479413+natwoj@users.noreply.github.com>
2021-10-25 21:42:03 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
 let playButton = document.getElementById("playButton");
 let pauseButton = document.getElementById("pauseButton");
 let videoPlayer = document.getElementById("videoPlayer");
-
-function changePlayButtonToPauseButton() {
-    playButton.classList.remove("play-containter-visible-on");
-    playButton.classList.add("play-containter-visible-off");
+// zmiana ikony play na pause po kliknięciu
+function ChangePlayButtonToPauseButton() {
+    playButton.classList.remove("play-container-visible-on");
+    playButton.classList.add("play-container-visible-off");
 
     pauseButton.classList.remove("pause-container-visible-off");
     pauseButton.classList.add("pause-container-visible-on");
@@ -13,4 +13,18 @@ function changePlayButtonToPauseButton() {
     function doPlayVideo(){
         videoPlayer.play();
     }
+}
+function ChangePauseButtonToPlayButton () {
+pauseButton.classList.remove("pause-container-visible-on");
+pauseButton.classList.add("pause-container-visible-off");
+
+playButton.classList.remove("play-container-visible-off");
+playButton.classList.add("play-container-visible-on");
+
+doPauseVideo();
+function doPauseVideo() {
+videoPlayer.pause(); 
+
+}
+
 }
